3363532600a7f9260ceefacab36629d8a4e1441c
DARKSPACE98 <enriquechuyin2019@gmail.com>
2023-03-30 22:55:15 -0700

    Respaldo de Hooks en React useFetch useTodo useForm useCounter

---
+++ useTodo.js
@@ -0,0 +1,64 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from '../08-useReducer/todoReducer';
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+  }
+
+export const useTodo = (initialState = []) => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify( todos ))
+    }, [todos])
+    
+  
+    const HandleNewTodo = (todo) =>{
+  
+      const action = {
+        type: '[TODO] Add Todo',
+        payload: todo
+      }
+  
+      dispatch(action);
+    
+    }
+  
+    const HandleDeleteTodo = (id) =>{
+      dispatch({
+        type:'[TODO] Remove Todo',
+        payload: id
+      })
+    
+    }
+  
+    const HandleToggleTodo = (id) =>{
+      dispatch({
+        type:'[TODO] Toggle Todo',
+        payload: id
+      })
+    }
+
+    const todosCount =
+
+        todos.length
+
+    
+
+    const pendingTodosCount =
+
+        todos.filter(todo=>!todo.done).length
+
+  return {
+    todos, 
+    HandleNewTodo,
+    HandleDeleteTodo,
+    HandleToggleTodo,
+    todosCount,
+    pendingTodosCount
+ }
+
+}
+
